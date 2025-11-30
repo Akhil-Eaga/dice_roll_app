5a86ccf7bc217c4daec1806f4f6dbfe53c3c7b59
Akhil Eaga <akhil.eaga@bhp.com>
2025-11-30 11:13:42 +0800

    Added a const constructor to the DiceRoller widget

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -4,6 +4,8 @@ import "package:flutter/material.dart";
 final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
   @override
   State<DiceRoller> createState() {
     return _DiceRollerState();
