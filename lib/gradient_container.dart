0473913e65034ab396fcd0f9713f9a3a5891fee2
Akhil Eaga <akhil.eaga@bhp.com>
2025-11-30 10:51:38 +0800

    Replaced styled_text with the image of a dice

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -28,8 +28,8 @@ class GradientContainer extends StatelessWidget {
           colors: colors,
         ),
       ),
-      child: const Center(
-        child: StyledText("Hello world!")
+      child: Center(
+        child: Image.asset("assets/images/dice-2.png", width: 200),
       ),
     );
   }
